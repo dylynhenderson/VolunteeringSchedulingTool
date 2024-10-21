b921caeecd220443345c5e6ddf60e0dc3994ba1f
Dylan Henderson <104692424+dylynhenderson@users.noreply.github.com>
2024-10-21 10:42:29 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for, redirect
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/membership')
+def membership():
+    return render_template('membership.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Processing For Invalid URL
+@app.route('/<userInput>')
+def reset(userInput):
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
